699e60797bc4c92066c3d81e74fe2dbabc96a692
Frankie <34449029+usagibryan@users.noreply.github.com>
2024-12-06 19:20:22 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from settings import *
+
+class Main:
+    def __init__(self):
+
+        # general
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+if __name__ -- '__main__':
+    main = Main()
+    
