27a495848401e14d9864216e488a685dce756861
Frankie <34449029+usagibryan@users.noreply.github.com>
2024-12-07 15:29:11 -0500

    Add comment reminder to preview.py

--- preview.py
+++ preview.py
@@ -2,6 +2,7 @@ from settings import *
 
 class Preview:
     def __init__(self):
+        # look at these next two lines again, compare to score.py, and understand why the math works
         self.surface = pygame.Surface((SIDEBAR_WIDTH,GAME_HEIGHT * PREVIEW_HEIGHT_FRACTION))
         self.rect = self.surface.get_rect(topright = (WINDOW_WIDTH - PADDING, PADDING))
         self.display_surface = pygame.display.get_surface() # return main window from main.py
